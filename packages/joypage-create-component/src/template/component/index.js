1c9e68e23c8963e25f8bb6c9c35703ddd71472e5
Liu zewei <543347844@qq.com>
2022-09-27 15:48:13 +0800

    122

--- packages/joypage-create-component/src/template/component/index.js
+++ packages/joypage-create-component/src/template/component/index.js
@@ -1,9 +1,8 @@
-import * as Vue from "vue";
-const _@CONST_COMPONENT_NAME@ = Vue.defineAsyncComponent(() => import(/* webpackChunkName: "async" */ /* webpackPrefetch:true */ "./component.vue"));
+const _@CONST_COMPONENT_NAME@ = import(/* webpackChunkName: "work.ui" */ /* webpackPrefetch:true */ "./component.vue");
 
 const @CONST_COMPONENT_NAME@ = Object.assign(_@CONST_COMPONENT_NAME@, {
     install: (app) => {
-        app.component(_@CONST_COMPONENT_NAME@.name, _@CONST_COMPONENT_NAME@);
+        app.component("@CONST_COMPONENT_NAME@", _@CONST_COMPONENT_NAME@);
     }
 });
 
