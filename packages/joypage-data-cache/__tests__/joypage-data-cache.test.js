96107014ec3f6c289feb5a6873c6f5a539a05191
874396180 <liupandeng@bicai365.com>
2022-10-26 22:54:19 +0800

    222

--- packages/joypage-data-cache/__tests__/joypage-data-cache.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const joypageDataCache = require("..");
-const assert = require("assert").strict;
-
-assert.strictEqual(joypageDataCache(), "Hello from joypageDataCache");
-console.info("joypageDataCache tests passed");
